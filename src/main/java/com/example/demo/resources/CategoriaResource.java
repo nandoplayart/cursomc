1ad47775ab9351c9cc370ee0271a7897aa5e5ace
luiz assis <luiz.fernando.desenvolvimento@outlook.com>
2019-08-04 19:41:25 -0300

    criando repository e service para categoria

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -3,26 +3,32 @@ package com.example.demo.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Categoria;
+import com.example.demo.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria>  listar() {
-		
-		Categoria c1 = new Categoria(1,"Informatica");
-		Categoria c2 = new Categoria(2,"Escritório");
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(c1);
-		lista.add(c2);
-		
-		
-		return lista;
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?>  find(@PathVariable Integer id) {
+		Categoria c1 = service.buscar(id);
+		return ResponseEntity.ok(c1);
+	}
+	
+	@RequestMapping(value="/categoria",method=RequestMethod.POST)
+	public ResponseEntity<?> inserir(@PathVariable Categoria categoria){
+		Categoria c1 = service.inserir(categoria);
+		return ResponseEntity.ok(c1);
 	}
 }
---
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -0,0 +1,26 @@
+package com.example.demo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Repositories.CategoriaRepository;
+import com.example.demo.domain.Categoria;
+
+@Service
+public class CategoriaService {
+	//injeção de dependencia é automatica acrescentado o atributo @Autowired
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+	 Optional<Categoria> categoria =repo.findById(id);
+	 return categoria.orElse(null);
+	}
+	
+	public Categoria inserir(Categoria categoria) {
+		return repo.save(categoria);
+	}
+}
