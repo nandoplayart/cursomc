5c6546aa4ae4e85df86283172c95794393826dc0
luiz assis <luiz.fernando.desenvolvimento@outlook.com>
2019-08-04 19:51:00 -0300

    criação operação de instanciação

--- src/main/java/com/example/demo/CursomcApplication.java
+++ src/main/java/com/example/demo/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.example.demo.Repositories.CategoriaRepository;
+import com.example.demo.domain.Categoria;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cate1 = new Categoria(null,"Informatica");
+		Categoria cate2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cate1,cate2));
+		
 	}
 
 }
